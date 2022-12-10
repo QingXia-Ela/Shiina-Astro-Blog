a874ed81c5311a8e8cfff251c4654fc5e2d99371
QingXia-Ela <Elahere@163.com>
2022-12-10 11:50:14 +0800

    feat: add vue framework

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-// import vue from '@astrojs/vue'
+import vue from '@astrojs/vue'
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shiinafan.top',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), vue()],
   vite: {
     resolve: {
       alias: [
