5fbbdc31e989a8ee7b3d52eb857085088a2a9a0a
QingXia-Ela <Elahere@163.com>
2022-12-22 17:07:51 +0800

    feat(BlogInfo): add recent article list
    fix(blog): fix blog page pagination show incorrent when using SSR mode

--- src/utils/SortArticle.ts
+++ src/utils/SortArticle.ts
@@ -1,6 +1,7 @@
+import type Frontmatter from '@/declare/Frontmatter';
 import type { MarkdownInstance } from 'astro'
 
-export default function (List: MarkdownInstance<Record<string, any>>[]): MarkdownInstance<Record<string, any>>[] {
+export default function (List: MarkdownInstance<Frontmatter>[]): MarkdownInstance<Frontmatter>[] {
   return List.sort((a, b) => {
     return (
       new Date(b.frontmatter.date).valueOf() -
