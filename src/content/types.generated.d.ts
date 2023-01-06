7b65cff3189839899cb8f497016a5c30a502bfde
QingXia-Ela <Elahere@163.com>
2023-01-06 10:42:40 +0800

    feat(posts): add post sync
    frontmatter didn't fill

--- src/content/types.generated.d.ts
+++ src/content/types.generated.d.ts
@@ -54,56 +54,56 @@ declare module 'astro:content' {
 "blog": {
 "Cypress-前端E2E测试工具初体验/Cypress-前端E2E测试工具初体验.md": {
   id: "Cypress-前端E2E测试工具初体验/Cypress-前端E2E测试工具初体验.md",
-  slug: "Cypress-前端E2E测试工具初体验/Cypress-前端E2E测试工具初体验",
+  slug: "cypress-前端e2e测试工具初体验/cypress-前端e2e测试工具初体验",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
 },
-"Hello-World/Hello-World.md": {
-  id: "Hello-World/Hello-World.md",
-  slug: "Hello-World/Hello-World",
+"Hello-World/Hello-World.mdx": {
+  id: "Hello-World/Hello-World.mdx",
+  slug: "hello-world/hello-world",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
 },
 "JS浅入学习笔记/JS浅入学习笔记.md": {
   id: "JS浅入学习笔记/JS浅入学习笔记.md",
-  slug: "JS浅入学习笔记/JS浅入学习笔记",
+  slug: "js浅入学习笔记/js浅入学习笔记",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
 },
 "Pinia-Vue全局状态管理的一种新方案/Pinia-Vue全局状态管理的一种新方案.md": {
   id: "Pinia-Vue全局状态管理的一种新方案/Pinia-Vue全局状态管理的一种新方案.md",
-  slug: "Pinia-Vue全局状态管理的一种新方案/Pinia-Vue全局状态管理的一种新方案",
+  slug: "pinia-vue全局状态管理的一种新方案/pinia-vue全局状态管理的一种新方案",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
 },
 "React制作音乐播放器日记总结-1/React制作音乐播放器日记总结-1.md": {
   id: "React制作音乐播放器日记总结-1/React制作音乐播放器日记总结-1.md",
-  slug: "React制作音乐播放器日记总结-1/React制作音乐播放器日记总结-1",
+  slug: "react制作音乐播放器日记总结-1/react制作音乐播放器日记总结-1",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
 },
 "React制作音乐播放器日记总结-2/React制作音乐播放器日记总结-2.md": {
   id: "React制作音乐播放器日记总结-2/React制作音乐播放器日记总结-2.md",
-  slug: "React制作音乐播放器日记总结-2/React制作音乐播放器日记总结-2",
+  slug: "react制作音乐播放器日记总结-2/react制作音乐播放器日记总结-2",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
 },
 "React制作音乐播放器日记总结-3/React制作音乐播放器日记总结-3.md": {
   id: "React制作音乐播放器日记总结-3/React制作音乐播放器日记总结-3.md",
-  slug: "React制作音乐播放器日记总结-3/React制作音乐播放器日记总结-3",
+  slug: "react制作音乐播放器日记总结-3/react制作音乐播放器日记总结-3",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
 },
 "Typescript学习笔记/Typescript学习笔记.md": {
   id: "Typescript学习笔记/Typescript学习笔记.md",
-  slug: "Typescript学习笔记/Typescript学习笔记",
+  slug: "typescript学习笔记/typescript学习笔记",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
@@ -124,7 +124,7 @@ declare module 'astro:content' {
 },
 "一个简单的React网易云音乐播放器/一个简单的React网易云音乐播放器.md": {
   id: "一个简单的React网易云音乐播放器/一个简单的React网易云音乐播放器.md",
-  slug: "一个简单的React网易云音乐播放器/一个简单的React网易云音乐播放器",
+  slug: "一个简单的react网易云音乐播放器/一个简单的react网易云音乐播放器",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
@@ -150,16 +150,16 @@ declare module 'astro:content' {
   collection: "blog",
   data: InferEntrySchema<"blog">
 },
-"团队学习方向介绍/团队学习方向介绍.md": {
-  id: "团队学习方向介绍/团队学习方向介绍.md",
+"团队学习方向介绍/团队学习方向介绍.mdx": {
+  id: "团队学习方向介绍/团队学习方向介绍.mdx",
   slug: "团队学习方向介绍/团队学习方向介绍",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
 },
-"基于ThreeJs实现的网页3D粒子效果经验分享/基于ThreeJs实现的网页3D粒子效果经验分享.md": {
-  id: "基于ThreeJs实现的网页3D粒子效果经验分享/基于ThreeJs实现的网页3D粒子效果经验分享.md",
-  slug: "基于ThreeJs实现的网页3D粒子效果经验分享/基于ThreeJs实现的网页3D粒子效果经验分享",
+"基于ThreeJs实现的网页3D粒子效果经验分享/基于ThreeJs实现的网页3D粒子效果经验分享.mdx": {
+  id: "基于ThreeJs实现的网页3D粒子效果经验分享/基于ThreeJs实现的网页3D粒子效果经验分享.mdx",
+  slug: "基于threejs实现的网页3d粒子效果经验分享/基于threejs实现的网页3d粒子效果经验分享",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
@@ -194,7 +194,7 @@ declare module 'astro:content' {
 },
 "记一次Gitblit-Jenkins实现前端自动化部署上线经历/记一次Gitblit-Jenkins实现前端自动化部署上线经历.md": {
   id: "记一次Gitblit-Jenkins实现前端自动化部署上线经历/记一次Gitblit-Jenkins实现前端自动化部署上线经历.md",
-  slug: "记一次Gitblit-Jenkins实现前端自动化部署上线经历/记一次Gitblit-Jenkins实现前端自动化部署上线经历",
+  slug: "记一次gitblit-jenkins实现前端自动化部署上线经历/记一次gitblit-jenkins实现前端自动化部署上线经历",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
@@ -206,23 +206,23 @@ declare module 'astro:content' {
   collection: "blog",
   data: InferEntrySchema<"blog">
 },
-"记一次前端Canvas粒子效果实战-1/记一次前端Canvas粒子效果实战-1.md": {
-  id: "记一次前端Canvas粒子效果实战-1/记一次前端Canvas粒子效果实战-1.md",
-  slug: "记一次前端Canvas粒子效果实战-1/记一次前端Canvas粒子效果实战-1",
+"记一次前端Canvas粒子效果实战-1/记一次前端Canvas粒子效果实战-1.mdx": {
+  id: "记一次前端Canvas粒子效果实战-1/记一次前端Canvas粒子效果实战-1.mdx",
+  slug: "记一次前端canvas粒子效果实战-1/记一次前端canvas粒子效果实战-1",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
 },
 "记一次前端Canvas粒子效果实战-2/记一次前端Canvas粒子效果实战-2.md": {
   id: "记一次前端Canvas粒子效果实战-2/记一次前端Canvas粒子效果实战-2.md",
-  slug: "记一次前端Canvas粒子效果实战-2/记一次前端Canvas粒子效果实战-2",
+  slug: "记一次前端canvas粒子效果实战-2/记一次前端canvas粒子效果实战-2",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
 },
 "记一次前端Canvas粒子效果实战-3/记一次前端Canvas粒子效果实战-3.md": {
   id: "记一次前端Canvas粒子效果实战-3/记一次前端Canvas粒子效果实战-3.md",
-  slug: "记一次前端Canvas粒子效果实战-3/记一次前端Canvas粒子效果实战-3",
+  slug: "记一次前端canvas粒子效果实战-3/记一次前端canvas粒子效果实战-3",
   body: string,
   collection: "blog",
   data: InferEntrySchema<"blog">
