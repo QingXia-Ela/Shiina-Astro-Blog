14676592c12e4bf29f542e9836d949d33e8d9382
QingXia-Ela <Elahere@163.com>
2023-02-09 20:23:51 +0800

    feat!: change blog content to template style

--- source/_inject/_page.js
+++ source/_inject/_page.js
@@ -1,32 +1,32 @@
 /** inject your script global */
 await import('./GlobalTest/index')
 
+const base = '/Shiina-Astro-Blog'
+
 /** 
  * inject player on index
  * 
  * Here we inject a music player on index and the core is from cdn
  */
-if (window.location.pathname == '/') {
+if (window.location.pathname == base + '/') {
   const h = document.querySelector('html'), p = document.createElement('script')
   /**
    * inject by `<script></script>` element because this progress couldn't finish in module mode
    * 
    * it's a sync progress
    */
-  p.setAttribute('src', 'https://unpkg.com/petite-vue@0.4.1/dist/petite-vue.iife.js')
+  p.setAttribute('src', base + '/js/petite-vue/pv.min.js')
   h.appendChild(p)
-  if (window.PetiteVue) {
-    await import('./MiniPlayer/index')
-    await import('./MiniPlayer/index.css')
-    const core = window._PlayerCore
-    const l = [{
-      id: 0,
-      name: 'I Got Love',
-      img: '/MiniPlayer/test.jpg',
-      src: '/MiniPlayer/test.mp3',
-    }]
-    for (const i of l) {
-      core.AppendSong(i)
-    }
+  await import('./MiniPlayer/index.js')
+  await import('./MiniPlayer/index.css')
+  const core = window._PlayerCore
+  const l = [{
+    id: 0,
+    name: 'I Got Love',
+    img: '/MiniPlayer/test.jpg',
+    src: '/MiniPlayer/test.mp3',
+  }]
+  for (const i of l) {
+    core.AppendSong(i)
   }
 }
